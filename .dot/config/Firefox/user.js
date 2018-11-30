4de81c9a47e7fdce86327a6565c196860fc4a237
Dave Avanthay <dave.avanthay@inf.ethz.ch>
2018-11-30 13:08:45 +0100

    add Firfox user.js, and dotfiles todo's

---
+++ .dot/config/Firefox/user.js
@@ -0,0 +1,6 @@
+user_pref("print.print_footerleft", "");
+user_pref("print.print_footercenter", "");
+user_pref("print.print_footerright", "");
+user_pref("print.print_headerleft", "");
+user_pref("print.print_headercenter", "");
+user_pref("print.print_headerright", "");
---
+++ .dot/todo.md
@@ -0,0 +1,4 @@
+# Dotfiles TODO's
+
+* [ ] Move `.install` into `.dot`
+* [ ] Remove Spectacle from being directly tracked, copy the default settings to `.dot/conf`
